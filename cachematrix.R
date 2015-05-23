05cb83d6855c71e9243886987f2a5db6e04be1e2
Jonah Glover <jonah@jonahglover.com>
2015-05-23 00:49:31 -0700

    working sketch

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,58 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## make a cache
+cache <- matrixCache()
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  cache$cache(x)
 }
 
 
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  cm <- cache$cache(x)
+  cm$inverse()
+}
+
+####################################################################################
+
+cacheMatrix <- function(x = matrix()){
+  inverse <- solve(x)
+  getInverse <- function(){
+    inverse
+  }
+  get <- function(){
+    x
+  }
+  list(  
+    get = get,
+    inverse = getInverse
+  )
+}
+
+# let x be a matrix with n cells
+matrixCache <- function() {
+  
+  matrixTable <- list()
+  
+  # O(n)
+  # hashed matrix looks like
+  # c1c2c3c4...cnnrow(n)ncol(n)
+  hashMatrix <- function(mat = matrix()){
+    hashStr <- vector()
+    for (cell in B){
+      hashStr <- paste(c(hashStr, cell), collapse="")
+    }
+    hashStr <- paste(c(hashStr, nrow(B)), collapse="")
+    hashStr <- paste(c(hashStr, ncol(B)), collapse="")
+    hashStr
+  }
+  
+  # returns a cacheMatrix of x
+  cache <- function(x = matrix()){
+    key <- hashMatrix(x)
+    if(is.null(matrixTable[[key]])){
+      matrixTable[[key]] <- cacheMatrix(x)
+    }
+    # matrixTable[[key]]
+  }
+  
+  list(cache=cache)
 }
